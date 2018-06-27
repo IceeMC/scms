be4c85183d653ad3f42080a36c3ba97710fd1c83
Victor Veytsman <vityavv@gmail.com>
2018-06-26 20:09:29 -0400

    Add a login system!!!!!!!!!!

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let fs = require("fs");
 //Imported modules - express: serve site; ejs: render HTML files
 let express = require("express");
 let ejs = require("ejs");
+let session = require("express-session");
 
 //Local modules - database.js: API for the articles db; config.json: configuration file
 let db = require("./database.js");
@@ -11,8 +12,25 @@ let config = require("./config.json");
 
 let app = express();//setup express
 app.use(express.json());
+app.use(session({secret: config.secret}));
 let cssfiles = fs.readdirSync("./templates/css");//read which CSS files to use
 app.use("/css", express.static("templates/css"));//serve static CSS
+
+//make an authenticator
+app.use((req, res, next) => {
+	if (!req.path.startsWith("/app") || req.path.startsWith("/app/login")) {
+		next();
+		return;
+	}
+	if (!req.session.username || !req.session.password) {
+		res.redirect("/app/login.html");
+		return;
+	}
+	db.login(req.session.username, req.session.password).then(el => {
+		if (el) next();
+		else res.redirect("/app/login.html");
+	});
+});
 
 app.get("/", (req, res) => {
 	//get five articles and convert date to a readable format
@@ -59,4 +77,19 @@ app.post("/api/insert", (req, res) => {
 	}
 });
 
-app.listen(8080);	
+app.get("/app/login.html", (req, res) => res.sendFile("appviews/login.html", {root: __dirname}));
+
+app.post("/app/login", (req, res) => {
+	if (req.body && req.body.username && req.body.password) {
+		db.login(req.body.username, req.body.password).then(el => {
+			if (el) {
+				req.session.username = req.body.username;
+				req.session.password = req.body.password;
+			}
+			req.session.save();
+			res.send(String(el));
+		});
+	} else res.send("false");
+});
+
+app.listen(8080);
